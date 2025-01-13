b13b418ad90249707ae8b159d143bf6e41cafee5
Zebb0a <156677733+Zebb0a@users.noreply.github.com>
2025-01-13 14:39:06 +0000

    3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -203,6 +203,22 @@ public class Main {
     public static String coordinatePunto(int x1, int y1, int x2, int y2,int x3,int y3) {
         String risposta="1,1";
         // TODO: SCRIVI QUI IL CODICE
-        return risposta;
+        int x4, y4;
+
+        if (x1 == x2) {
+            x4 = x3;
+        } else if (x1 == x3) {
+            x4 = x2; 
+        } else {
+            x4 = x1; 
+        }
+        if (y1 == y2) {
+            y4 = y3;
+        } else if (y1 == y3) {
+            y4 = y2;
+        } else {
+            y4 = y1; 
+        }
+        return x4 + "," + y4;
     }
 }
